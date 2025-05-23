4f3029427b56d3817af83e74da593dbb9067cadc
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2025-05-23 02:24:06 +0200

    Ajout de la configuration Tailwind CSS et des thèmes personnalisés, incluant des couleurs, des ombres, des polices, des animations et des keyframes. Mise à jour des styles globaux pour intégrer les nouvelles variables de thème.

---
+++ theme/animation.ts
@@ -0,0 +1,13 @@
+export const Customanimation = {
+    "animate-25s": "animate 25s linear infinite",
+    marquee: "marquee 30s linear infinite",
+  };
+
+  export const Customkeyframes = {
+    marquee: {
+      "0%": { transform: "translateX(0%)" },
+      "100%": { transform: "translateX(-50%)" },
+    },
+  };
+
+  
