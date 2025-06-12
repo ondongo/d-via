54c87468a3778af451ec684c32f307c7cde65ab2
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2025-06-12 12:24:19 +0200

    Modification de la fonction handleClick dans le composant BottomActionButton pour rediriger vers les pages appropriées : vers "/coming" si l'utilisateur est sur la page artisan, et vers "/dashboard/clients" sinon. Mise à jour du texte du bouton pour refléter correctement l'action en fonction de la page actuelle.

--- src/components/organisms/BottomActionButton.tsx
+++ src/components/organisms/BottomActionButton.tsx
@@ -10,11 +10,12 @@ export default function BottomActionButton() {
 
   const handleClick = () => {
     if (isOnArtisanPage) {
-      router.push("/");
+      router.push("/coming");
     } else {
-      router.push("/artisans");
+      router.push("/dashboard/clients");
     }
   };
+
   return (
     <div className="fixed bottom-0 left-0 w-full bg-dvianeutral-96 px-4 py-6  z-[99999] md:hidden flex justify-center">
       <button
@@ -22,10 +23,9 @@ export default function BottomActionButton() {
         className="text-white text-label-large leading-label-large tracking-label-large bg-dviaprimary-40 px-4 py-1 shadow-lg border rounded-8px border-transparent text-sm font-medium
                hover:shadow-sm transition-shadow duration-300 cursor-pointer max-w-[260px]  h-[40px]"
       >
-        
         {isOnArtisanPage
-          ? "Commencer en tant que client"
-          : "Commencer en tant qu'artisan"}
+          ? "Commencer en tant qu'artisan "
+          : "Commencer en tant que client"}
       </button>
     </div>
   );
